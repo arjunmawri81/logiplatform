447c6ba7f944a4b5bab102717c3e054213bd2f41
Arjun Singh Mawri <arjunmawri@gmail.com>
2026-06-10 20:48:12 +0530

    connect frontend with deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://logiplatform-5eyx-git-main-arjunmawri81s-projects.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
